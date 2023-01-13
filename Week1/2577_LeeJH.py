4479d60abc3632ada23ebe1d372d8abadde8463d
Jun-Hyeok Lee <bluehyena123@naver.com>
2023-01-13 17:04:29 +0900

    [FIX] - update remark

--- Week1/2577_LeeJH.py
+++ Week1/2577_LeeJH.py
@@ -1,11 +1,15 @@
+# input 받기
 a = int(input())
 b = int(input())
 c = int(input())
 
+# string 으로 변환
 s_abc = str(a * b * c)
 
+# 정답 벡터 초기화
 blank = [0,0,0,0,0,0,0,0,0,0]
 
+# 각 값별 정답벡터에 값 추가 및 결과 출력
 for i in s_abc:
     blank[int(i)] += 1
 for i in range(10):
--- Week1/5378_LeeJH.py
+++ Week1/5378_LeeJH.py
@@ -4,10 +4,12 @@ num = int(sys.stdin.readline())
 data = []
 ans = []
 
+# 덩치를 map 형태로 입력
 for i in range(num):
     a, b = map(int, input().split())
     data.append((a,b))
 
+# a,b값 모두가 클때를 기준으로 비교하여 값 갱신
 for i in range(num):
     tmp = 1
     for j in range(num):
@@ -15,5 +17,6 @@ for i in range(num):
             tmp += 1
     ans.append(tmp)
 
+# 결과 출력
 for a in ans:
     print(a, end=" ")
